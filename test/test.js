f536c980108a216e3dbe45ee38b606d45d59b5a4
magicbowen <e.bowen.wang@icloud.com>
2018-05-31 15:16:54 +0800

    fix test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
 
-describe('Placeholder', () => {
-  describe('#placeholder1', () => {
-    it('should return -1 when the value is not present', () => {
+describe('Placeholder', function () {
+  describe('#placeholder1', function () {
+    it('should return -1 when the value is not present', function () {
       assert.equal([1,2,3].indexOf(4), -1);
     });
   });
